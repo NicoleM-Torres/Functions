53cb4db3a666c8863f01bd2c400a805dadfd9964
Nicole Torres <nicole.torres@mscoding.org>
2024-08-12 13:51:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -71,8 +71,9 @@ function checkBalance() {
 
 //#endregion
 
-//#region Simple To-Do List
-//VARIABLES
+//#region Simple To Do List
+
+// VARIABLES
 let task_arr = [
   "Clean Bathroom",
   "Finish Homework",
@@ -81,35 +82,71 @@ let task_arr = [
   "Workout",
 ];
 
-//ADD TASK AND DISPLAY TO-DO LIST
+// DISPLAY TO CONSOLE
+console.log("Initial To-Do List:");
+displayTasks();
+
+addTask("Go grocery shopping"); // Add a task
+removeTask("Finish Homework"); // Remove an existing task
+
+// ADD TASK AND DISPLAY TO-DO LIST
 function addTask(task) {
-  if (task && typeof task === "string" && task !== "") {
-    let isTaskOnList = false;
-    for (let i = 0; i < task_arr.length; i++) {
-      if (task_arr[i] === task) isTaskOnList = true;
-      break;
-    } //END FOR LOOP
-  } //END OF IF STATEMENT
+  // Check if the task is a non-empty string
+    if (task && typeof task === "string" && task !== "") {
+        let isTaskOnList = false;
 
-  if (!isTaskOnList) {
-    task_arr.push(task);
-    console.log("Task added successfully!");
-  } else {
-    console.log("ERROR: Task is invalid.");
-  } //END IF-ELSE
-  displayTasks();
-} //END OF addTask FUNCTION
+        // Check for repeated tasks
+        for (let i = 0; i < task_arr.length; i++) {
+            if (task_arr[i] === task) {
+                isTaskOnList = true;
+                break;
+            } //END IF STATEMENT
+        } //END FOR LOOP
 
-//REMOVE TASK AND DISPLAY TO DO LIST
+        // Add the task if new
+        if (!isTaskOnList) {
+        task_arr.push(task);
+        console.log("Task added to your list!");
+        } else {
+        console.log("ERROR: Task already exists.");
+        } //END IF ELSE STATEMENT THAT ADDS NEW TASK
+    } else {
+        console.log("ERROR: Invalid task.");
+    } // END IF-ELSE STATEMENT THAT CHECKS STRINGS
+    displayTasks();
+} // END OF addTask FUNCTION
+
+// REMOVE TASK AND DISPLAY TO-DO LIST
 function removeTask(task) {
-let taskFound = false;
-for (let i =0; i < task_arr.length; i++) {
-    
-}
-} //END OF removeTask FUNCTION
+  let isTaskRemoved = false;
 
-//#endregion
+  // remove task if found on list
+  for (let i = 0; i < task_arr.length; i++) {
+    if (task_arr[i] === task) {
+      task_arr.splice(i, 1); //.splice removed if string is already in the list
+      isTaskRemoved = true;
+      break;
+    } //END IF STATEMENT
+  } //END FOR LOOP
 
-//#region Temp Converter
+  /*  Display to console based on if the task is on the list
+    if it is on the list it will display completed if not it will say 
+    not on the list` */
+
+  if (isTaskRemoved) {
+    console.log("Task completed!");
+  } else {
+    console.log("Task not on list.");
+  } //END IF-ELSE STATEMENT
+  displayTasks();
+} // END OF removeTask FUNCTION
+
+// DISPLAY TO-DO LIST
+function displayTasks() {
+  console.log("To-Do List:");
+  for (let i = 0; i < task_arr.length; i++) {
+    console.log(`${i + 1}. ${task_arr[i]}`);
+  } //END FOR LOOP
+} //END DISPLAY TASK FUNCTION
 
 //#endregion
